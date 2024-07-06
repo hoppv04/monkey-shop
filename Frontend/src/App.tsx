9c1b3a6a796101a2ef61cd8a47e984798a72fa57
hoppv <phamhopxd1@gmail.com>
2024-07-06 08:58:39 +0700

    Khởi tạo dự án và cài đặt thư viện

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <button className="btn btn-primary">Button bootstrap</button>
+    </>
+  );
+}
+
+export default App;
